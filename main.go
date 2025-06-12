650ef10b0f07299895518a5db641312bc934e601
Joseph Sebastian <josephsv96@gmail.com>
2025-06-13 00:21:26 +0530

    server accepts OTLP requests via http (only proto payload)

--- main.go
+++ main.go
@@ -2,12 +2,60 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 
 	_ "github.com/marcboeker/go-duckdb/v2"
+	"go.opentelemetry.io/collector/pdata/plog/plogotlp"
 
 	"github.com/alkmst-xyz/sweetcorn/internal/logs"
 )
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "OK\n")
+}
+
+type Service struct {
+	ctx           context.Context
+	db            *sql.DB
+	insertLogsSQL string
+}
+
+func unmarshalLogsRequest(buf []byte) (plogotlp.ExportRequest, error) {
+	req := plogotlp.NewExportRequest()
+	err := req.UnmarshalProto(buf)
+	return req, err
+}
+
+func (s Service) handleLogs(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Fatalf("Failed to read request body: %v", err)
+		return
+	}
+	if err = r.Body.Close(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Fatalf("Failed to close request body: %v", err)
+		return
+	}
+
+	otlpReq, err := unmarshalLogsRequest(body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Fatalf("Failed to unmarshal request: %v", err)
+		return
+	}
+
+	newLogs := otlpReq.Logs()
+	if err := logs.InsertLogsData(s.ctx, s.db, s.insertLogsSQL, newLogs); err != nil {
+		log.Fatalf("Failed to write logs to db: %v", err)
+		return
+	}
+}
 
 func main() {
 	cfg := &logs.Config{
@@ -27,10 +75,17 @@ func main() {
 		log.Fatalf("failed to create table: %v", err)
 	}
 
-	new_logs := logs.SimpleLogs(1)
 	insertLogsSQL := logs.RenderInsertLogsSQL(cfg)
 
-	if err := logs.InsertLogsData(ctx, db, insertLogsSQL, new_logs); err != nil {
-		log.Fatalf("insertLog failed: %v", err)
+	svc := &Service{
+		ctx:           ctx,
+		db:            db,
+		insertLogsSQL: insertLogsSQL,
 	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("POST /v1/logs", svc.handleLogs)
+
+	http.ListenAndServe("localhost:8090", mux)
 }
