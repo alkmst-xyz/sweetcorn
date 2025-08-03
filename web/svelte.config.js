c743fccfcfac03e12c52308d06e926b7253cc6b5
Joseph Sebastian <josephsv96@gmail.com>
2025-08-03 16:54:27 +0200

    feat(ui): use hash-based routing #23

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({ fallback: 'index.html' }) }
+	kit: {
+		adapter: adapter({ fallback: 'index.html' }),
+		router: {
+			type: 'hash'
+		}
+	}
 };
 
 export default config;
