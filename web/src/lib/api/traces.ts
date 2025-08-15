7d081b7527b5803721429866e21dfb515ac6267c
Joseph Sebastian <josephsv96@gmail.com>
2025-08-15 12:55:56 +0200

    relocate api type definitions

--- web/src/lib/api/traces.ts
+++ web/src/lib/api/traces.ts
@@ -1,38 +1,5 @@
-import type { FetchType } from './types';
+import type { FetchType, ServicesResponse, TraceRecord } from './types';
 import { API_BASE } from './utils';
-
-export type TraceRecord = {
-	timestamp: string;
-	traceId: string;
-	spanId: string;
-	parentSpanId: string;
-	traceState: string;
-	spanName: string;
-	spanKind: string;
-	serviceName: string;
-	resourceAttributes: { [key: string]: unknown };
-	scopeName: string;
-	scopeVersion: string;
-	spanAttributes: { [key: string]: unknown };
-	duration: number;
-	statusCode: string;
-	statusMessage: string;
-	eventsTimestamps: number[];
-	eventsNames: string[];
-	eventsAttributes: { [key: string]: unknown }[];
-	linksTraceIds: string[];
-	linksSpanIds: string[];
-	linksTraceStates: string[];
-	linksAttributes: { [key: string]: unknown }[];
-};
-
-export type ServicesResponse = {
-	data: string;
-	errors?: unknown;
-	limit: number;
-	offset: number;
-	total: number;
-};
 
 export async function getTraces(fetch: FetchType) {
 	const res = await fetch(`${API_BASE}/v1/traces`);
--- web/src/lib/api/types.ts
+++ web/src/lib/api/types.ts
@@ -1,1 +1,88 @@
 export type FetchType = typeof fetch;
+
+export type TraceRecord = {
+	timestamp: string;
+	traceId: string;
+	spanId: string;
+	parentSpanId: string;
+	traceState: string;
+	spanName: string;
+	spanKind: string;
+	serviceName: string;
+	resourceAttributes: { [key: string]: unknown };
+	scopeName: string;
+	scopeVersion: string;
+	spanAttributes: { [key: string]: unknown };
+	duration: number;
+	statusCode: string;
+	statusMessage: string;
+	eventsTimestamps: number[];
+	eventsNames: string[];
+	eventsAttributes: { [key: string]: unknown }[];
+	linksTraceIds: string[];
+	linksSpanIds: string[];
+	linksTraceStates: string[];
+	linksAttributes: { [key: string]: unknown }[];
+};
+
+// Jaeger Query API
+export type ServicesResponse = {
+	data: string[];
+	errors: any;
+	limit: number;
+	offset: number;
+	total: number;
+};
+
+export type TraceKeyValuePair = {
+	key: string;
+	type: string;
+	value: any;
+};
+
+export type TraceProcess = {
+	serviceName: string;
+	tags: TraceKeyValuePair[];
+};
+
+export type TraceSpanReference = {
+	refType: string;
+	spanID: string;
+	traceID: string;
+};
+
+export type TraceLog = {
+	timestamp: number;
+	fields: TraceKeyValuePair[];
+	name: string;
+};
+
+export type Span = {
+	traceID: string;
+	spanID: string;
+	processID: string;
+	operationName: string;
+	startTime: number;
+	duration: number;
+	logs: TraceLog[];
+	references: TraceSpanReference[];
+	tags: TraceKeyValuePair[];
+	warnings: string[];
+	flags: number;
+	stackTraces: string[];
+};
+
+export type TraceResponse = {
+	processes: { [key: string]: TraceProcess };
+	traceID: string;
+	warnings: string[];
+	spans: Span[];
+};
+
+export type TracesResponse = {
+	data: TraceResponse[];
+	errors: any;
+	limit: number;
+	offset: number;
+	total: number;
+};
